4c64595dcca6cd40e0b6f566f6fc9a3dbbca7d52
Stephen Gadd <stephen@docuracy.co.uk>
2025-07-04 17:12:17 +0100

    Actions Deployment

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,4 +2,8 @@
 export default {
   base: '/Historical_Sea_Routing/', // Required for GitHub Pages
   publicDir: 'public',
+  build: {
+    outDir: '../docs', // build into the root-level 'docs/' folder
+    emptyOutDir: true
+  }
 };
